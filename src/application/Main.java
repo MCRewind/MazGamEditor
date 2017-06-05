bf3d3eb455d0f86e08bd115d93ee1abf2afad995
Clayton Meyer <sploing1@gmail.com>
2017-06-05 13:11:20 -0700

    large ui test, fake registration page

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,135 +1,161 @@
 package application;
 
-import javafx.animation.PathTransition; 
 import javafx.application.Application; 
-import static javafx.application.Application.launch; 
-import javafx.event.EventHandler; 
+import javafx.collections.FXCollections; 
+import javafx.collections.ObservableList; 
 
-import javafx.scene.Group; 
+import javafx.geometry.Insets; 
+import javafx.geometry.Pos; 
+
 import javafx.scene.Scene; 
 import javafx.scene.control.Button; 
-import javafx.scene.input.MouseEvent; 
-import javafx.scene.paint.Color; 
-
-import javafx.scene.shape.Circle; 
-import javafx.scene.shape.LineTo; 
-import javafx.scene.shape.MoveTo; 
-import javafx.scene.shape.Path; 
+import javafx.scene.control.CheckBox; 
+import javafx.scene.control.ChoiceBox; 
+import javafx.scene.control.DatePicker; 
+import javafx.scene.control.ListView; 
+import javafx.scene.control.RadioButton; 
+import javafx.scene.layout.GridPane; 
+import javafx.scene.text.Text; 
+import javafx.scene.control.TextField; 
+import javafx.scene.control.ToggleGroup;  
+import javafx.scene.control.ToggleButton; 
 import javafx.stage.Stage; 
-import javafx.util.Duration; 
          
 public class Main extends Application { 
    @Override 
-   public void start(Stage stage) {      
-      //Drawing a Circle 
-      Circle circle = new Circle(); 
+   public void start(Stage stage) {    
+      //Label for name 
+      Text nameLabel = new Text("Name"); 
       
-      //Setting the position of the circle 
-      circle.setCenterX(300.0f); 
-      circle.setCenterY(135.0f); 
-      
-      //Setting the radius of the circle 
-      circle.setRadius(25.0f);  
-      
-      //Setting the color of the circle 
-      circle.setFill(Color.BROWN); 
-      
-      //Setting the stroke width of the circle 
-      circle.setStrokeWidth(20);      
+      //Text field for name 
+      TextField nameText = new TextField(); 
        
-      //Creating a Path 
-      Path path = new Path(); 
+      //Label for date of birth 
+      Text dobLabel = new Text("Date of birth"); 
       
-      //Moving to the staring point 
-      MoveTo moveTo = new MoveTo(208, 71);               
-      
-      //Creating 1st line 
-      LineTo line1 = new LineTo(421, 161);        
-      
-      //Creating 2nd line 
-      LineTo line2 = new LineTo(226,232); 
-      
-      //Creating 3rd line 
-      LineTo line3 = new LineTo(332,52);        
-      
-      //Creating 4th line 
-      LineTo line4 = new LineTo(369, 250);        
-      
-      //Creating 5th line 
-      LineTo line5 = new LineTo(208, 71);       
-      
-      //Adding all the elements to the path 
-      path.getElements().add(moveTo); 
-      path.getElements().addAll(line1, line2, line3, line4, line5);     
-      
-      //Creating the path transition 
-      PathTransition pathTransition = new PathTransition(); 
-      
-      //Setting the duration of the transition 
-      pathTransition.setDuration(Duration.millis(1000));       
-      
-      //Setting the node for the transition 
-      pathTransition.setNode(circle); 
-      
-      //Setting the path for the transition 
-      pathTransition.setPath(path); 
-      
-      //Setting the orientation of the path 
-      pathTransition.setOrientation(
-         PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-      
-      //Setting the cycle count for the transition 
-      pathTransition.setCycleCount(50); 
-      
-      //Setting auto reverse value to true 
-      pathTransition.setAutoReverse(false);
-      
-      //Creating play button 
-      Button playButton = new Button("Play"); 
-      playButton.setLayoutX(300); 
-      playButton.setLayoutY(250); 
+      //date picker to choose date 
+      DatePicker datePicker = new DatePicker(); 
        
-      circle.setOnMouseClicked (new EventHandler<javafx.scene.input.MouseEvent>() { 
-         @Override 
-         public void handle(javafx.scene.input.MouseEvent e) { 
-            System.out.println("Hello World"); 
-            circle.setFill(Color.DARKSLATEBLUE);             
-         } 
-      });   
-      playButton.setOnMouseClicked((new EventHandler<MouseEvent>() { 
-         public void handle(MouseEvent event) { 
-            System.out.println("Hello World");  
-            pathTransition.play(); 
-         } 
-      })); 
-       
-      //Creating stop button 
-      Button stopButton = new Button("stop"); 
-      stopButton.setLayoutX(250); 
-      stopButton.setLayoutY(250); 
+      //Label for gender
+      Text genderLabel = new Text("gender"); 
       
-      stopButton.setOnMouseClicked((new EventHandler<MouseEvent>() { 
-         public void handle(MouseEvent event) { 
-            System.out.println("Hello World"); 
-            pathTransition.stop(); 
-         } 
-      }));
-      //Creating a Group object  
-      Group root = new Group(circle, playButton, stopButton); 
-         
+      //Toggle group of radio buttons       
+      ToggleGroup groupGender = new ToggleGroup(); 
+      RadioButton maleRadio = new RadioButton("male"); 
+      maleRadio.setToggleGroup(groupGender); 
+      RadioButton femaleRadio = new RadioButton("female"); 
+      femaleRadio.setToggleGroup(groupGender); 
+       
+      //Label for reservation 
+      Text reservationLabel = new Text("Reservation"); 
+      
+      //Toggle button for reservation 
+      ToggleButton Reservation = new ToggleButton(); 
+      ToggleButton yes = new ToggleButton("Yes"); 
+      ToggleButton no = new ToggleButton("No"); 
+      ToggleGroup groupReservation = new ToggleGroup(); 
+      yes.setToggleGroup(groupReservation);   
+      no.setToggleGroup(groupReservation); 
+       
+      //Label for technologies known 
+      Text technologiesLabel = new Text("Technologies Known"); 
+      
+      //check box for education 
+      CheckBox javaCheckBox = new CheckBox("Java"); 
+      javaCheckBox.setIndeterminate(false); 
+      
+      //check box for education 
+      CheckBox dotnetCheckBox = new CheckBox("DotNet"); 
+      javaCheckBox.setIndeterminate(false); 
+       
+      //Label for education 
+      Text educationLabel = new Text("Educational qualification"); 
+      
+      //list View for educational qualification 
+      ObservableList<String> names = FXCollections.observableArrayList( 
+         "Engineering", "MCA", "MBA", "Graduation", "MTECH", "Mphil", "Phd"); 
+      ListView<String> educationListView = new ListView<String>(names); 
+      
+      //Label for location 
+      Text locationLabel = new Text("location"); 
+      
+      //Choice box for location 
+      ChoiceBox locationchoiceBox = new ChoiceBox(); 
+      locationchoiceBox.getItems().addAll
+         ("Hyderabad", "Chennai", "Delhi", "Mumbai", "Vishakhapatnam"); 
+       
+      //Label for register 
+      Button buttonRegister = new Button("Register");  
+      
+      //Creating a Grid Pane 
+      GridPane gridPane = new GridPane();    
+      
+      //Setting size for the pane 
+      gridPane.setMinSize(500, 500); 
+       
+      //Setting the padding    
+      gridPane.setPadding(new Insets(10, 10, 10, 10));  
+      
+      //Setting the vertical and horizontal gaps between the columns 
+      gridPane.setVgap(5); 
+      gridPane.setHgap(5);       
+      
+      //Setting the Grid alignment 
+      gridPane.setAlignment(Pos.CENTER); 
+       
+      //Arranging all the nodes in the grid 
+      gridPane.add(nameLabel, 0, 0); 
+      gridPane.add(nameText, 1, 0); 
+       
+      gridPane.add(dobLabel, 0, 1);       
+      gridPane.add(datePicker, 1, 1); 
+      
+      gridPane.add(genderLabel, 0, 2); 
+      gridPane.add(maleRadio, 1, 2);       
+      gridPane.add(femaleRadio, 2, 2); 
+      gridPane.add(reservationLabel, 0, 3); 
+      gridPane.add(yes, 1, 3);       
+      gridPane.add(no, 2, 3);  
+       
+      gridPane.add(technologiesLabel, 0, 4); 
+      gridPane.add(javaCheckBox, 1, 4);       
+      gridPane.add(dotnetCheckBox, 2, 4);  
+       
+      gridPane.add(educationLabel, 0, 5); 
+      gridPane.add(educationListView, 1, 5);      
+       
+      gridPane.add(locationLabel, 0, 6); 
+      gridPane.add(locationchoiceBox, 1, 6);    
+       
+      gridPane.add(buttonRegister, 2, 8);      
+      
+      //Styling nodes   
+      buttonRegister.setStyle(
+         "-fx-background-color: darkslateblue; -fx-textfill: white;"); 
+       
+      nameLabel.setStyle("-fx-font: normal bold 15px 'serif' "); 
+      dobLabel.setStyle("-fx-font: normal bold 15px 'serif' "); 
+      genderLabel.setStyle("-fx-font: normal bold 15px 'serif' "); 
+      reservationLabel.setStyle("-fx-font: normal bold 15px 'serif' "); 
+      technologiesLabel.setStyle("-fx-font: normal bold 15px 'serif' "); 
+      educationLabel.setStyle("-fx-font: normal bold 15px 'serif' "); 
+      locationLabel.setStyle("-fx-font: normal bold 15px 'serif' "); 
+       
+      //Setting the back ground color 
+      gridPane.setStyle("-fx-background-color: BEIGE;");       
+       
       //Creating a scene object 
-      Scene scene = new Scene(root, 600, 300); 
-      scene.setFill(Color.LAVENDER);  
+      Scene scene = new Scene(gridPane); 
       
       //Setting title to the Stage 
-      stage.setTitle("Convenience Methods Example");  
+      stage.setTitle("Registration Form"); 
          
       //Adding scene to the stage 
-      stage.setScene(scene); 
-         
+      stage.setScene(scene);  
+      
       //Displaying the contents of the stage 
       stage.show(); 
-   } 
+   }      
    public static void main(String args[]){ 
       launch(args); 
    } 
